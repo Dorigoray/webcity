13e2a2de4975a6f2deb9336c4007783756f1e890
Tofu <tofu455067@gmail.com>
2023-10-16 21:36:35 +0700

    git commit -m "dsada" dsadsa

---
+++ JavaS/task2/script.js
@@ -0,0 +1,28 @@
+//Обозначние переменных/констант 
+const consolelog = document.querySelector('#consoleLog');
+const mAlert = document.querySelector('#alert');
+const mPrompt = document.querySelector('#prompt');
+const Primer1 = document.querySelector('#Primer1');
+const Primer2 = document.querySelector('#Primer2');
+const Primer3 = document.querySelector('#Primer3');
+
+//block Alert
+consolelog.addEventListener('click',() => {
+alert('метод для ввода сообщения в консоль');
+});
+mAlert.addEventListener('click',() => {
+alert('метод для отображения информации пользователям ');
+});
+mPrompt.addEventListener('click',() => {
+alert('метод для отображения диалогового окна с запросом на ввод текста');
+});
+ //block Событий        
+ Primer1.addEventListener('click',() => {
+console.log('запись в консоле');
+ })
+ Primer2.addEventListener('click',() => {
+    alert('Вот так работает метод!!!!');
+})    
+ Primer3.addEventListener('click',() => {
+    prompt('Запишите хоть-что');
+})    
